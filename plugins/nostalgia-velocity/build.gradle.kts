e621000c3a934fa9557f5943923ff70a608f0611
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-03-18 13:04:46 +0000

    Start work on Velocity plugin

---
+++ plugins/nostalgia-velocity/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("kapt")
+}
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    kapt("com.velocitypowered:velocity-api:3.1.1")
+
+    implementation(project(":libraries:nostalgia-rpc"))
+}
